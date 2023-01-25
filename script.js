7068837b2810b17b3d197ce3a498288e7b511afe
Vincent Clarysse <claryssevincent@gmail.com>
2023-01-25 12:00:01 +0100

    add createcard function

--- script.js
+++ script.js
@@ -6,33 +6,41 @@ const foo = async () => {
         const data = await result.json()
         console.log(data);
 
-        const weatherdescription = document.createElement("p");
-        weathercard.appendChild(weatherdescription);
-        weatherdescription.innerText = data.list[0].weather[0].description;
-
-        const weathercard=document.createElement("div");
-        weathercard.className="weathercard";
-        wrapper.appendChild(weathercard);
-
-        const city_name = document.createElement("h2");
-        weathercard.appendChild(city_name);
-        city_name.innerText = data.city.name;
-
-        const current_temp = document.createElement("p");
-        weathercard.appendChild(current_temp);
-        current_temp.innerText = data.list[0].main.temp + " °C"
-        
-        const min_temp = document.createElement("p");
-        weathercard.appendChild(min_temp);
-        min_temp.innerText = data.list[0].main.temp_min + " °C"
-
-        const max_temp = document.createElement("p");
-        weathercard.appendChild(max_temp);
-        max_temp.innerText = data.list[0].main.temp_max + " °C"
+        createcard(data);
         
     } catch (err) {
         console.error(err)
     }
+}
+
+const createcard = (data) => {
+    const weathercard=document.createElement("div");
+    weathercard.className="weathercard";
+    wrapper.appendChild(weathercard);
+
+    const icon = document.createElement("img");
+    weathercard.appendChild(icon);
+    icon.src="http://openweathermap.org/img/wn/"+ data.list[0].weather[0].icon +"@2x.png"
+
+    const city_name = document.createElement("h2");
+    weathercard.appendChild(city_name);
+    city_name.innerText = data.city.name;
+
+    const weatherdescription = document.createElement("p");
+    weathercard.appendChild(weatherdescription);
+    weatherdescription.innerText = data.list[0].weather[0].description;
+
+    const current_temp = document.createElement("p");
+    weathercard.appendChild(current_temp);
+    current_temp.innerText = data.list[0].main.temp + " °C"
+    
+    const min_temp = document.createElement("p");
+    weathercard.appendChild(min_temp);
+    min_temp.innerText = data.list[0].main.temp_min + " °C"
+
+    const max_temp = document.createElement("p");
+    weathercard.appendChild(max_temp);
+    max_temp.innerText = data.list[0].main.temp_max + " °C"
 }
 
 foo();
